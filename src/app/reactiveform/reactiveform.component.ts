8d276ae2e72ba28809c07815ef57a23e1c49a4a2
sathvik-pothuneedi <2000031331@kluniversity.in>
2022-12-29 16:31:11 +0530

    Reactive forms

--- src/app/reactiveform/reactiveform.component.ts
+++ src/app/reactiveform/reactiveform.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-reactiveform',
   templateUrl: './reactiveform.component.html',
   styleUrls: ['./reactiveform.component.css']
 })
-export class ReactiveformComponent {
+export class ReactiveformComponent implements OnInit{
   student:any;
+  ngOnInit(): void {
+
+    this.student=new FormGroup({
+    firstname : new FormControl("",Validators.compose([
+      Validators.required,
+      Validators.maxLength(10),
+      Validators.minLength(3),
+      Validators.pattern('^[a-zA-Z]*$')
+
+    ])),
+    lastname : new FormControl("",Validators.compose([
+      Validators.required,
+      Validators.maxLength(10),
+      Validators.minLength(3),
+
+    ])),
+    age : new FormControl("",Validators.compose([
+      Validators.required,
+      Validators.min(18),
+      Validators.max(100),
+      Validators.pattern(/^[0-9]\d*$/)
+
+    ])),
+
+  
+  })
+}
 }
